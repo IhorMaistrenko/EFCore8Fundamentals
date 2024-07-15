40eb31886d64b8f1a87255ebc65225ae82cdd6dc
Ihor Maistrenko <19951137+IhorMaistrenko@users.noreply.github.com>
2024-07-15 21:23:05 +0200

    Reading and Writing some data

--- M02 Building Your FIrst App/Before/PublisherConsole/Program.cs
+++ M02 Building Your FIrst App/Before/PublisherConsole/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using PublisherData;
+using PublisherDomain;
+
+using (PubContext context = new())
+{
+    context.Database.EnsureCreated();
+}
+
+GetAuthors();
+//AddAuthor();
+GetAuthors();
+
+void AddAuthor()
+{
+    using var context = new PubContext();
+    var author = new Author { FirstName = "Ryan", LastName = "Gosling" };
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+
+void GetAuthors()
+{
+    using var context = new PubContext();
+    var authors = context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+
+}
