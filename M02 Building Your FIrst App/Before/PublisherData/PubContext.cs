563457c90d6521f80d6df9efc1011cd9bc72a74f
Ihor Maistrenko <19951137+IhorMaistrenko@users.noreply.github.com>
2024-07-15 21:12:06 +0200

    Add PublisherData project

---
+++ M02 Building Your FIrst App/Before/PublisherData/PubContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
+
+namespace PublisherData;
+
+public class PubContext: DbContext
+{
+    public DbSet<Author> Authors { get; set; }  
+    public DbSet<Book> Books { get; set; }
+ 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer("Server=localhost,1433;Database = PubDatabase;User ID=;Password=;TrustServerCertificate=YES");
+    }
+}
